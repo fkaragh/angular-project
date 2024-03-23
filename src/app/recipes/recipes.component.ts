8cbcd320447465d4fdfabf6ee04b59aa848a9b63
fkaragh <fkfurkankara@gmail.com>
2024-03-23 13:53:17 +0300

    Some event emitters changed to Subjects

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import { Recipe } from './recipe.model';
+
 import { RecipeService } from './recipe.service';
-import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-recipes',
@@ -10,16 +9,9 @@ import { ActivatedRoute } from '@angular/router';
   providers: [RecipeService]
 })
 export class RecipesComponent {
-  selectedRecipe : Recipe;
-  
-  constructor(private recipeService: RecipeService, private route: ActivatedRoute){ }
+  constructor(){ }
 
   ngOnInit(){
-    this.recipeService.recipeSelected
-    .subscribe(
-      (recipe: Recipe) => {
-        this.selectedRecipe = recipe;
-      }
-    );
+
   }
 }
